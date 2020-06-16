543c19a0df6f52c1656599f7b8afb30ecead3b73
Holeryn <Holeryn@protonmail.com>
2020-06-16 20:52:00 +0200

    added inb

--- barebones/cursor/io.s
+++ barebones/cursor/io.s
@@ -1,11 +1,17 @@
-    .global outb
+    global outb
 
-    #  outb -  send a byte to an I/O port
-    #  stack:  [esp + 8] the data byte
-    #          [esp + 4] the I/O port
-    #          [esp]     return address
+    ;;  outb -  send a byte to an I/O port
+    ;;  stack:  [esp + 8] the data byte
+    ;;          [esp + 4] the I/O port
+    ;;          [esp]     return address
 outb:
-    MOV (esp + 8),%AL            # al = the data type
-    MOV (esp + 4),%DX            # dx = the I/O port
-    OUT %AL,%DX                  # Write at port [dx] th value of [al]
-    RET
+    mov al,[esp + 8]            ; al = the data type
+    mov dx,[esp + 4]            ; dx = the I/O port
+    out dx,al                   ; Write at port [dx] th value of [al]
+    ret
+
+    global inb
+inb:
+    mov	dx,[esp + 4]
+    in 	al,dx
+    ret
