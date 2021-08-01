28183ee30b00cf0a2396242a70b9381ac808c983
Holeryn <Holeryn@protonmail.com>
2021-08-01 22:22:39 +0200

    Fixed the compile script

--- barebones/compile.sh
+++ barebones/compile.sh
@@ -5,7 +5,6 @@ AS=i686-elf-as
 ${AS} boot.s -o boot.o
 ${AS} io.s -o io.o
 ${AS} segmentation/lgdt.s -o segmentation/lgdt.o
-${AS} segmentation/reloadsegments.s -o segmentation/reloadsegments.o
 
 ${CC} -c cursor/cursor.c -o cursor.o
 ${CC} -c kernel.c -o kernel.o ${CC_FLAGS} -ffreestanding
@@ -13,7 +12,7 @@ ${CC} -c serial/serial.c -o serial.o ${CC_FLAGS} -ffreestanding
 ${CC} -c memory/memory.c -o memory/memory.o ${CC_FLAGS} -ffreestanding
 ${CC} -c terminal/terminal.c -o terminal/terminal.o ${CC_FLAGS} -ffreestanding
 ${CC} -c segmentation/segmentation.c -o segmentation/segmentation.o ${CC_FLAGS} -ffreestanding
-${CC} -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o io.o cursor.o kernel.o serial.o memory/memory.o terminal/terminal.o segmentation/lgdt.o segmentation/reloadsegments.o segmentation/segmentation.o -L libcc.a
+${CC} -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o io.o cursor.o kernel.o serial.o memory/memory.o terminal/terminal.o segmentation/lgdt.o segmentation/segmentation.o -L libcc.a
 
 if [ ! -d "isodir" ]; then
 	mkdir isodir
